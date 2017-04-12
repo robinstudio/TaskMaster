0ee89c3269fd748796cf371cdc1e05a903d526cc
Dongnan <1005021307@qq.com>
2017-04-12 23:41:54 +0800

    Life

--- app/src/main/java/com/android/dongnan/taskmaster/MainFragment.java
+++ app/src/main/java/com/android/dongnan/taskmaster/MainFragment.java
@@ -1,31 +1,98 @@
 package com.android.dongnan.taskmaster;
 
 import android.app.Fragment;
+import android.content.Context;
+import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.android.dongnan.taskmaster.data.DataItem;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by dream on 16/7/25.
  */
 public class MainFragment extends Fragment {
+    private static final String TAG = MainFragment.class.getSimpleName();
 
-    private ListView mListView;
+    private LayoutInflater mLayoutInflater;
+    private DataAdapter mDataAdapter;
+    private ArrayList<DataItem> mDataItems;
 
     public MainFragment() {
+        mDataItems = new ArrayList<>();
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_main, null);
-        mListView = (ListView)view.findViewById(R.id.listView);
+        mLayoutInflater = inflater;
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        ListView listView = (ListView)view.findViewById(R.id.listView);
+        mDataAdapter = new DataAdapter();
+        listView.setAdapter(mDataAdapter);
+        updateData();
         return view;
+    }
+
+    public void updateData() {
+        for(int i=0; i<10; i++) {
+            mDataItems.add(DataItem.getInstance().setTitle("Title-" + i)
+                .setContent("Content-" + i));
+        }
+        mDataAdapter.notifyDataSetChanged();
+    }
+
+    private class DataAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return mDataItems.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mDataItems.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder;
+            if (convertView == null) {
+                convertView = mLayoutInflater.inflate(R.layout.data_view, parent, false);
+                holder = new ViewHolder();
+                holder.title = (TextView)convertView.findViewById(R.id.title);
+                holder.content = (TextView)convertView.findViewById(R.id.content);
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder)convertView.getTag();
+            }
+
+            DataItem item = mDataItems.get(position);
+            holder.title.setText(item.getTitle());
+            holder.content.setText(item.getContent());
+            return convertView;
+        }
+    }
+
+    private static class ViewHolder {
+        public TextView title;
+        public TextView content;
     }
 }
