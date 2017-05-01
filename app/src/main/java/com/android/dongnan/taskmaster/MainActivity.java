929d6b61127762c48116c8642ce830a49ec2d5f2
Dongnan <1005021307@qq.com>
2017-05-01 22:14:37 +0800

    Add View

--- app/src/main/java/com/android/dongnan/taskmaster/MainActivity.java
+++ app/src/main/java/com/android/dongnan/taskmaster/MainActivity.java
@@ -1,21 +1,72 @@
 package com.android.dongnan.taskmaster;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 
-public class MainActivity extends Activity {
+import com.android.dongnan.taskmaster.data.CommonHolder;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MainFragment mMainFragment;
+    private EditFragment mEditFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        CommonHolder.setApplicationContext(this.getApplicationContext());
+
         setContentView(R.layout.activity_main);
 
-        getFragmentManager().beginTransaction().replace(R.id.container,
-                new MainFragment()).commit();
+        ViewPager viewPager = (ViewPager)findViewById(R.id.container);
+        viewPager.setAdapter(new StoryPager(getSupportFragmentManager()));
+        viewPager.setCurrentItem(0);
+    }
 
-        View view = findViewById(R.id.container);
-        view.setOnClickListener(v->{});
+    @Override
+    protected void onDestroy() {
+        CommonHolder.setApplicationContext(null);
+        super.onDestroy();
+    }
 
+    private class StoryPager extends FragmentPagerAdapter {
+
+        private StoryPager(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            if (position == 0) {
+                if (mMainFragment == null) {
+                    mMainFragment = new MainFragment();
+                }
+                mMainFragment.updateData();
+                return mMainFragment;
+            } else if (position == 1) {
+                if (mEditFragment == null) {
+                    mEditFragment = new EditFragment();
+                }
+                mEditFragment.updateView();
+                return mEditFragment;
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
